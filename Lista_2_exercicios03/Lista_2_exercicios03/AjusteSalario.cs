9b5d0ebd5c1141f82b457a99d61a3b4bf722d665
lipex90 <phillipemarques45@gmail.com>
2020-12-17 19:38:56 -0300

    Lista_2_exercicio03

---
+++ Lista_2_exercicios03/Lista_2_exercicios03/AjusteSalario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista_2_exercicios03
+{
+    class AjusteSalario
+    {
+        public double  Salario { get; set; }
+        public string Funcionario { get; set; }
+
+        public double Reajuste { get; set; }
+
+        public AjusteSalario()
+        {
+
+        }
+
+        public AjusteSalario(double salario, string funcionario, double reajuste)
+        {
+            Salario = salario;
+            Funcionario = funcionario;
+            Reajuste = reajuste;
+        }
+       public void SalarioReajuse()
+        {
+           Reajuste = Reajuste / 100;
+        }
+        public void SalarioAumento()
+        {
+            Salario *= Reajuste;
+            return;
+        }
+
+
+    }
+}
---
+++ Lista_2_exercicios03/Lista_2_exercicios03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lista_2_exercicios03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            AjusteSalario salario = new AjusteSalario();
+            AjusteSalario funcionario = new AjusteSalario();
+            AjusteSalario reajuste = new AjusteSalario();
+
+            Console.WriteLine("Digite o nome do funcionário");
+            funcionario.Funcionario = Console.ReadLine();
+            Console.WriteLine("Digite quantidade do salário");
+            salario.Salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantos por cento para reajustar o salario");
+            reajuste.Reajuste = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome : "+funcionario.Funcionario);
+            Console.WriteLine("Salario : "+salario.Salario);
+            Console.WriteLine("Reajuse : "+reajuste.Reajuste);
+        }
+    }
+}
