2ec90d669b07c5552dc9140c1bc502fb8927e2be
lipex90 <phillipemarques45@gmail.com>
2020-12-17 18:59:34 -0300

    Fixacao02_01

---
+++ Lista_2_exercícios/Lista_2_exercícios/CalculoDia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista_2_exercícios
+{
+    class CalculoDia
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int Ano { get; set; }
+
+        public CalculoDia() 
+        {
+        }
+
+        public CalculoDia(string nome, int idade, int ano)
+        {
+            Nome = nome;
+            Idade = idade;
+            Ano = ano;
+        }
+        public void Anodevida(int ano,int idade)
+        {
+            ano = idade * 365;
+            return ;
+        }
+    }
+}
---
+++ Lista_2_exercícios/Lista_2_exercícios/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lista_2_exercícios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculoDia nome = new CalculoDia();
+            CalculoDia idade = new CalculoDia();
+            CalculoDia ano = new CalculoDia();
+
+            Console.WriteLine("Digite seu primeiro nome");
+            nome.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade você tem");
+            idade.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(nome.Nome+" VOCÊ JÁ VIVEU "+ano.Ano+" DIAS");
+        }
+    }
+}
